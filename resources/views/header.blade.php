18276b44020e2c9eb7514d53f64b16d07af4857e
Yuta Aikawa <715-student016@la-study.com>
2026-06-22 10:37:24 +0900

    headerのレイアウトを調整

--- resources/views/header.blade.php
+++ resources/views/header.blade.php
@@ -1,9 +1,25 @@
+<head>
+    <style>
 
+        header {
+            display: flex;
+            flex-direction: column;
+            text-align: center;
+        }
 
+        header .categories {
+            display: flex;
+            flex-direction: row;
+            justify-content: center;
+            gap: 10px;
+        }
+
+    </style>
+</head>
 
 <header>
-        <h1>SAY YOU!!ユージーン店</h1>
-        <nav>
+    <h1 class="shop-title">SAY YOU!!ユージーン店</h1>
+    <nav>
         <form action="/products/search" method="post">
             @csrf
             <input type="text" name="keyword" placeholder="商品名" value="{{ old('keyword') }}">
@@ -16,7 +32,7 @@
             <div class="category">
                 <a href="/products">全商品</a>
             </div>
-            
+
             @foreach ($categories as $category)
             <div class="category">
                 <a href="/products?categoryId={{ $category['id'] }}"
@@ -26,7 +42,6 @@
             </div>
             @endforeach
         </div>
-    
+
         <a href="/cart" style="padding-left: 10px;">カートを見る</a>
 </header>
-
