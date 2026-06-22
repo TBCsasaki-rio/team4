dbd384e3f93d6e93690b09bc357671dc90a587ed
Yuta Aikawa <715-student016@la-study.com>
2026-06-22 15:13:19 +0900

    サイドバーの条件検索機能：実装完了（テスト不足）

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -75,35 +75,47 @@ class ProductController extends Controller
     public function conditionSearch(Request $request)
     {
         $price_ranges = $request->input('filter-price-range',[]);
+        $category_ids = $request->input('filter-category', []);
 
-        if (count($price_ranges) === 0){
+        if (count($price_ranges) === 0 and count($category_ids) === 0){
             return redirect("/products");
         }
 
         // パースして有効なレンジ集める
         $parsed = [];
-        foreach ($price_ranges as $price_range){
-            // 0to499 -> 0, 499 に分けて、変数に格納
-            [$minprice, $maxprice] = explode('to', $price_range);
-            // is_numeric() 数字であるかを検査
-            $minprice = is_numeric($minprice) ? (int)$minprice : null;
-            $maxprice = is_numeric($maxprice) ? (int)$maxprice : null;
-            // 配列に要素追加
-            $parsed[] = ['minprice' => $minprice, 'maxprice' => $maxprice];
+        $query = Product::query();
+
+        if (count($price_ranges) > 0) {
+            foreach ($price_ranges as $price_range){
+                // 0to499 -> 0, 499 に分けて、変数に格納
+                [$minprice, $maxprice] = explode('to', $price_range);
+                // is_numeric() 数字であるかを検査
+                $minprice = is_numeric($minprice) ? (int)$minprice : null;
+                $maxprice = is_numeric($maxprice) ? (int)$maxprice : null;
+                // 配列に要素追加
+                $parsed[] = ['minprice' => $minprice, 'maxprice' => $maxprice];
+
+                $query->where(function ($q) use ($parsed) {
+                    foreach ($parsed as $range){
+                        if ($range['maxprice'] === null){
+                            $q->orWhere('price', '>=', $range['minprice']);
+                            } else {
+                                $q->orWhereBetween('price', [$range['minprice'], $range['maxprice']]);
+                            }
+                    }
+                });
+            }
         }
 
-        $query = Product::query();
-        $query->where(function ($q) use ($parsed) {
-            foreach ($parsed as $range){
-                if ($range['maxprice'] === null){
-                    $q->orWhere('price', '>=', $range['minprice']);
-                } else {
-                    $q->orWhereBetween('price', [$range['minprice'], $range['maxprice']]);
-                }
+        if (count($category_ids) > 0) {
+            foreach($category_ids as $category_id){
+                $query->where(function ($q) use ($category_id){
+                    $q->orWhere('category_id', $category_id);
+                });
             }
-        });
-
-        $products = $query->orderBy('price')->get();
+        }
+        
+        $products = $query->get();
         $categories = Category::get();
 
         //デバッグ用にコメントアウトしています。
