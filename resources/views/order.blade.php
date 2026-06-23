9859fd3c5cb91dbb265ecd76789b36d1d85c3db9
Seina Hamada <715-student025@la-study.com>
2026-06-23 09:19:37 +0900

    購入者情報入力画面に「カート画面に戻る」ボタンを追加

--- resources/views/order.blade.php
+++ resources/views/order.blade.php
@@ -6,67 +6,71 @@
 </head>
 
 <body>
-    <main>
-        <h1>
-            <img src="{{ asset('images/order.jpg') }}" alt="orderLogo" style="max-width: 250px; height:auto;">
-        </h1>
+    <div class="container">
+        <main>
+            <h1>
+                <img src="{{ asset('images/order.jpg') }}" alt="orderLogo" style="max-width: 250px; height:auto;">
+            </h1>
 
 
-        <hr>
+            <hr>
 
-        <h3>お客様情報を入力してください</h3>
+            <h3>お客様情報を入力してください</h3>
 
-        {{-- ✅ エラー表示 --}}
-        @if ($errors->any())
-            <div style="color:red;">
-                <ul>
-                    @foreach ($errors->all() as $error)
-                        <li>{{ $error }}</li>
-                    @endforeach
-                </ul>
+            {{-- ✅ エラー表示 --}}
+            @if ($errors->any())
+                <div style="color:red;">
+                    <ul>
+                        @foreach ($errors->all() as $error)
+                            <li>{{ $error }}</li>
+                        @endforeach
+                    </ul>
+                </div>
+            @endif
+
+            {{-- ✅ ルート変更（Laravel controllerへ） --}}
+            <form action="/order" method="post">
+                @csrf
+
+                <table>
+                    <tr>
+                        <th>お名前</th>
+                        <td>
+                            <input type="text" name="name" value="{{ old('name') }}">
+                        </td>
+                    </tr>
+                    <tr>
+                        <th>住所</th>
+                        <td>
+                            <input type="text" name="address" value="{{ old('address') }}">
+                        </td>
+                    </tr>
+                    <tr>
+                        <th>電話番号</th>
+                        <td>
+                            <input type="text" name="tel" value="{{ old('tel') }}">
+                        </td>
+                    </tr>
+                    <tr>
+                        <th>e-mail</th>
+                        <td>
+                            <input type="email" name="email" value="{{ old('email') }}">
+                        </td>
+                    </tr>
+                </table>
+
+            <div class="button">
+                <button type="submit">
+                    注文する（合計：{{ number_format($total) }}円）
+                </button>
             </div>
-        @endif
 
-        {{-- ✅ ルート変更（Laravel controllerへ） --}}
-        <form action="/order" method="post">
-            @csrf
+            <div style="text-align: center; margin-top: 20px;">
+                <a href="{{ route('cart') }}">カート画面に戻る</a>
+            </div>
 
-            <table border="1">
-                <tr>
-                    <th>お名前</th>
-                    <td>
-                        <input type="text" name="name" value="{{ old('name') }}">
-                    </td>
-                </tr>
-                <tr>
-                    <th>住所</th>
-                    <td>
-                        <input type="text" name="address" value="{{ old('address') }}">
-                    </td>
-                </tr>
-                <tr>
-                    <th>電話番号</th>
-                    <td>
-                        <input type="text" name="tel" value="{{ old('tel') }}">
-                    </td>
-                </tr>
-                <tr>
-                    <th>e-mail</th>
-                    <td>
-                        <input type="email" name="email" value="{{ old('email') }}">
-                    </td>
-                </tr>
-            </table>
-
-            
-        <button type="submit">
-             注文する（合計：{{ number_format($total) }}円）
-        </button>
-
-        </form>
-    </main>
-
-    <hr>
-
+            </form>
+        </main>
+        @include('footer')
+    </div>
 </body>
-@include('footer')
