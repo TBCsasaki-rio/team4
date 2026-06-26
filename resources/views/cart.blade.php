a790130a2bea9e94f30a1bff06b72a848fd56d68
Yuta Aikawa <715-student016@la-study.com>
2026-06-26 09:24:29 +0900

    微修正：カート画面でタブアイコンが変更されていなかったので修正しました。

--- resources/views/cart.blade.php
+++ resources/views/cart.blade.php
@@ -5,6 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>カート</title>
+    <link rel="icon" href="{{ asset('images/home_logo.jpg') }}" type="image/jpeg">
     <link rel="stylesheet" href="{{ asset('css/cart.css') }}">
 </head>
 
